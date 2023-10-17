f3e5ec6cee610feb539069d298bcc78206e465e1
devantonio27 <antoniodev2709@gmail.com>
2023-10-17 12:13:49 -0300

    função  lampada quebrada

--- lampada.js
+++ lampada.js
@@ -1,13 +1,20 @@
 var lamp = window.document.getElementById('lampada')
 
+function estaQuebrada(){
+    return lamp.src.indexOf('quebrada') > -1
+}
+
 function Ligar(){
-    lamp.src = './img/ligada.svg'
+    if (!estaQuebrada()){
+    lamp.src = './img/ligada.svg'}
 }
 
 function Desligar(){
-    lamp.src = './img/desligada.svg'
+    if (!estaQuebrada()){ 
+    lamp.src = './img/desligada.svg'}
 }
 
+lamp.addEventListener ('click', Quebrar)
 function Quebrar(){
     lamp.src = './img/quebrada.svg'
 }
