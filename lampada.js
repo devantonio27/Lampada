81c6f3ef4b4640864a20f617ba76aa092991323d
devantonio27 <antoniodev2709@gmail.com>
2023-10-17 12:04:37 -0300

    funcionalidade JS adicionada

---
+++ lampada.js
@@ -0,0 +1,13 @@
+var lamp = window.document.getElementById('lampada')
+
+function Ligar(){
+    lamp.src = './img/ligada.svg'
+}
+
+function Desligar(){
+    lamp.src = './img/desligada.svg'
+}
+
+function Quebrar(){
+    lamp.src = './img/quebrada.svg'
+}
